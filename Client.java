7d9198b3b9fc06a42693676de8e2a1046a9ae047
WernerLove <73615240+Wernelove@users.noreply.github.com>
2023-09-25 12:30:38 -0300

    Update Client.java
    Mudança completa na logica, agora é baseado em Threads

--- Client.java
+++ Client.java
@@ -4,95 +4,102 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.MulticastSocket;
 import java.net.NetworkInterface;
-import java.text.SimpleDateFormat;
 import java.util.Scanner;
-import java.util.Arrays;
-import java.util.Date;
 
-public class Client {
-   public Client() {
+public class Client implements Runnable {
+
+   int topic;
+   String username;
+   int port;
+   InetAddress grupo;
+   MulticastSocket socket;
+   String Ip = "230.0.0.0";
+   
+   public Client(int topic, String username) throws IOException {
+      this.topic = topic;
+      this.username = username;
+
+      if (topic == 1) {
+          this.port = 4321;
+          grupo = InetAddress.getByName(Ip);
+      } else if(topic == 2){
+          this.port = 4322;
+          grupo = InetAddress.getByName(Ip);
+      } else{
+         this.port = 4323;
+          grupo = InetAddress.getByName(Ip);
+      }
+
+      InetAddress ia = InetAddress.getByName(Ip);
+      InetSocketAddress grupo = new InetSocketAddress(ia, port);
+      socket = new MulticastSocket(port);
+      NetworkInterface ni = NetworkInterface.getByInetAddress(ia);
+
+      socket.joinGroup(grupo, ni);
+  }
+
+   public void run() {
+
    }
+
+   void enviarMensagem(String mensagem, String username){
+      String msg = String.format("[Client | $s] $s: ", getTime(), username + mensagem);
+      byte[] envio = new byte[1024];
+      envio = msg.getBytes();
+
+      try {
+         DatagramPacket pacote = new DatagramPacket(envio, envio.length, grupo, port);
+         socket.send(pacote);
+      } catch (IOException e) {
+         e.printStackTrace();
+     }
+   }
+
+   
+
+   String getTime() {
+      return java.time.LocalTime.now().toString().substring(0, 5);
+  }
 
    public static void main(String[] args) throws IOException {
 
-      // Iniciar variavel para armazenar mensagens
-      String msg = " ";
-      String def_grupo = " ";
       Scanner sc = new Scanner(System.in);
 
-      // Criar socket multicast para receber na porta 4321
-      MulticastSocket socket = new MulticastSocket(4321);
+      System.out.print("Informe seu nome de usuário:");
+      String username = sc.nextLine();
 
-      System.out.print("Defina os grupos que irá participar: ");
-      def_grupo = sc.nextLine();
-      String[] def_grupo_split = def_grupo.split(" ");
+      System.out.print("Escolha a sala de bate papo:\n1 - Filmes\n2 - Series\n3 - Novidades");
+      int topico = Integer.parseInt(sc.nextLine());
 
+      Client info = new Client(topico, username);
+      Thread receiverThread = new Thread(info);
+      receiverThread.start();
 
-     if(Arrays.asList(def_grupo_split).contains("grupo1")){
-         
-         // Definir IP do host
-         InetAddress ia1 = InetAddress.getByName("230.0.0.0");
+      String msg = " ";
+      String mensagem = " ";
+      MulticastSocket socket;
+      socket = new MulticastSocket(info.port);
 
-         // Combinar o IP com o port 
-         InetSocketAddress grupo1 = new InetSocketAddress(ia1, 4321);
+      while(!mensagem.contains("Sair")) {
 
-         // Encontra a interface de rede do IP e salvar numa variavel
-         NetworkInterface ni1 = NetworkInterface.getByInetAddress(ia1);
+         System.out.print("Digite a sua mensagem:");
+         mensagem = sc.nextLine();
+        	info.enviarMensagem(mensagem, username);
 
-         // Se conectar ao grupo definido
-         socket.joinGroup(grupo1, ni1); 
-
-      } else if(Arrays.asList(def_grupo_split).contains("grupo2")){
-         
-         InetAddress ia2 = InetAddress.getByName("231.0.0.0");
-         InetSocketAddress grupo2 = new InetSocketAddress(ia2, 4322);
-         NetworkInterface ni2 = NetworkInterface.getByInetAddress(ia2);
-         socket.joinGroup(grupo2, ni2); 
-
-      } else if(Arrays.asList(def_grupo_split).contains("grupo3")){
-         
-         InetAddress ia3 = InetAddress.getByName("232.0.0.0");
-         InetSocketAddress grupo3 = new InetSocketAddress(ia3, 4323);
-         NetworkInterface ni3 = NetworkInterface.getByInetAddress(ia3);
-         socket.joinGroup(grupo3,ni3);
-
-      } else{
-         System.out.print("Nenhum grupo selecionado");
-      }
-
-      // Definir formatação do horário
-      SimpleDateFormat formatoDataHora = new SimpleDateFormat("HH:mm:ss");
-
-      // Receber mensagens do servidor enquanto não receber "Servidor Encerrado!"
-      while(!msg.contains("Servidor Encerrado!")) {
-        
-         // Iniciar sistema de horário 
-         Date horaAtualClient = new Date();
-         String horaFormatadaClient = formatoDataHora.format(horaAtualClient);
-
-         // Mensagem padrão do servidor e aguardar input
-         System.out.println("[Cliente | "+ horaFormatadaClient +"] Esperando por mensagem Multicast...");
-
-         // Criar buffer de dados para armazenar a mensagem
+         System.out.println("[Cliente] Esperando por mensagem Multicast...");
          byte[] buffer = new byte[1024];
-
-         // Criar pacote
          DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-
-         // Receber pacote e salvar numa variavel
          socket.receive(packet);
          msg = new String(packet.getData());
-
-         Date horaAtualServer = new Date();
-         String horaFormatadaServer = formatoDataHora.format(horaAtualServer);
-
-         // Printar mensagem recebida
-         System.out.println("[Cliente | "+ horaFormatadaServer + "] Mensagem recebida do Servidor: " + msg);
+         System.out.println("[Cliente] Mensagem recebida do Servidor: "+ msg);
+         if(!msg.contains("Servidor Encerrado!")){
+            break;
+         }
       }
 
-      // Desconectar do servidor
+      socket.leaveGroup(info.grupo);
       System.out.println("[Cliente] Conexao Encerrada!");
-      socket.leaveGroup(grupo1, ni1);
       socket.close();
+      sc.close();
    }
 }
